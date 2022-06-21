6f97dcf97ade65b30e9baefdc0af762c5721ca19
Luis Montes <montes_luisalberto79@hotmail.com>
2022-06-20 19:59:27 -0500

    Renombrado de activity, se agregaron permisos a Manifest, se creo clase Aplicacion para Hilt.
    Signed-off-by: Luis Montes <montes_luisalberto79@hotmail.com>

---
+++ app/src/main/java/montes/luis/mireddit/app/Aplicacion.kt
@@ -0,0 +1,10 @@
+package montes.luis.mireddit.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Aplicacion : Application(){
+
+}
+
--- app/src/main/java/montes/luis/mireddit/ui/actividades/PrincipalActivity.kt
+++ app/src/main/java/montes/luis/mireddit/ui/actividades/PrincipalActivity.kt
@@ -1,9 +1,10 @@
-package montes.luis.mireddit
+package montes.luis.mireddit.ui.actividades
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import montes.luis.mireddit.R
 
-class MainActivity : AppCompatActivity() {
+class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
